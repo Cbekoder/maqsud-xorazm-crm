6c9c4027624e67927c771dd31cf55ffa308c441d
mumrbekkk <madatovumrbek8@gmail.com>
2025-08-18 13:21:42 +0500

    Qr code [initial]

--- models/AttendanceModel.py
+++ models/AttendanceModel.py
@@ -6,13 +6,19 @@ from .LessonModel import Lesson
 
 
 class Attendance(models.Model):
+    ATTENDANCE_STATUS_CHOICES = [
+        ("Present", "Present"),
+        ("Absent", "Absent"),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="attendances")
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     came_at = models.DateTimeField(blank=True, null=True)
     left_at = models.DateTimeField(blank=True, null=True)
-    is_present = models.BooleanField(default=False)
+    status = models.CharField(max_length=50, choices=ATTENDANCE_STATUS_CHOICES, default=ATTENDANCE_STATUS_CHOICES[0][0])
     comment = models.TextField(blank=True, null=True)
 
     class Meta:
+        unique_together = ("user", "lesson")
         app_label = 'common'
 
