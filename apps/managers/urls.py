aab9b55602fc25ea3462233476a97cf0549ba950
cbekoder <cbekoder@gmail.com>
2025-08-20 00:28:54 +0500

    QR code showing and scanning initial commit

--- apps/managers/urls.py
+++ apps/managers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ManagerHomeView
+from .views import ManagerHomeView, QRScannerView, check_scanned_QR
 
 urlpatterns = [
     path('', ManagerHomeView.as_view(), name='ManagerHome'),
+    path("scanner/", QRScannerView.as_view(), name="qr_scanner"),
+    path("scanner/check/", check_scanned_QR, name="check_scanned_QR"),
 ]
